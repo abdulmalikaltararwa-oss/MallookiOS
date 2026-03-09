40facabb012f718532b738b900c8fe4a9c704fb4
abdulmalikaltararwa-oss <abdulmalikaltararwa@gmail.com>
2026-03-09 09:55:10 +0300

    Delete src directory

--- src/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('api', {
-  cacheLoad:  ()     => ipcRenderer.invoke('cache-load'),
-  cacheSave:  (json) => ipcRenderer.invoke('cache-save', json),
-  exportData: (json) => ipcRenderer.invoke('export-data', json),
-  importData: ()     => ipcRenderer.invoke('import-data'),
-});
