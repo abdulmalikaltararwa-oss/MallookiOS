23388efb7dfcdcff58730205013af9caa0bc492c
abdulmalikaltararwa-oss <abdulmalikaltararwa@gmail.com>
2026-03-10 14:37:36 +0300

    Expose updater API in preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,7 +1,15 @@
 const { contextBridge, ipcRenderer } = require('electron');
+
 contextBridge.exposeInMainWorld('api', {
   cacheLoad:  ()     => ipcRenderer.invoke('cache-load'),
   cacheSave:  (json) => ipcRenderer.invoke('cache-save', json),
   exportData: (json) => ipcRenderer.invoke('export-data', json),
   importData: ()     => ipcRenderer.invoke('import-data'),
+});
+
+contextBridge.exposeInMainWorld('updater', {
+  onEvent: (callback) => {
+    ipcRenderer.on('updater:event', (_event, data) => callback(data));
+  },
+  install: () => ipcRenderer.invoke('updater:install')
 });
